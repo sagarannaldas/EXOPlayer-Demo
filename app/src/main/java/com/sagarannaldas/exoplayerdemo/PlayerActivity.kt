00ec367fd016729460151b508c3a3ed00f405919
sagarannaldas <annal7057@gmail.com>
2025-03-09 20:35:52 +0530

    1. Added playback state  listener and events to control the player

--- app/src/main/java/com/sagarannaldas/exoplayerdemo/PlayerActivity.kt
+++ app/src/main/java/com/sagarannaldas/exoplayerdemo/PlayerActivity.kt
@@ -1,6 +1,7 @@
 package com.sagarannaldas.exoplayerdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
@@ -8,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.common.util.Util
 import androidx.media3.exoplayer.ExoPlayer
@@ -20,6 +22,8 @@ class PlayerActivity : AppCompatActivity() {
     private var playWhenReady = true
     private var currentItem = 0
     private var playbackPosition = 0L
+
+    private val playbackStateListener: Player.Listener = playbackStateListener()
 
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityPlayerBinding.inflate(layoutInflater)
@@ -90,22 +94,24 @@ class PlayerActivity : AppCompatActivity() {
             .also { exoPlayer ->
                 viewBinding.videoView.player = exoPlayer
 
+                // DASH media
                 val mediaItem = MediaItem.Builder()
                     .setUri(getString(R.string.media_url_dash))
                     .setMimeType(MimeTypes.APPLICATION_MPD)
                     .build()
                 exoPlayer.addMediaItem(mediaItem)
 
-                // media url for mp4
-//                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
-//                exoPlayer.setMediaItem(mediaItem)
-
                 // media url for mp3
-//                val secondMedia = MediaItem.fromUri(getString(R.string.media_url_mp3))
-//                exoPlayer.addMediaItem(secondMedia)
+                val secondMedia = MediaItem.fromUri(getString(R.string.media_url_mp3))
+                exoPlayer.addMediaItem(secondMedia)
+
+                // media url for mp4
+                val thirdMedia = MediaItem.fromUri(getString(R.string.media_url_mp4))
+                exoPlayer.setMediaItem(thirdMedia)
 
                 exoPlayer.playWhenReady = playWhenReady
                 exoPlayer.seekTo(currentItem, playbackPosition)
+                exoPlayer.addListener(playbackStateListener)
                 exoPlayer.prepare()
             }
     }
@@ -115,8 +121,46 @@ class PlayerActivity : AppCompatActivity() {
             playWhenReady = exoPlayer.playWhenReady
             currentItem = exoPlayer.currentMediaItemIndex
             playbackPosition = exoPlayer.currentPosition
+            exoPlayer.removeListener(playbackStateListener)
             exoPlayer.release()
         }
         player = null
+    }
+}
+
+private const val TAG = "PlayerActivity"
+
+private fun playbackStateListener() = object : Player.Listener {
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        val stateString: String = when (playbackState) {
+            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE"
+            ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING"
+            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY"
+            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED"
+            else -> "UNKNOWN STATE"
+        }
+        Log.d(TAG, "Changed state to: $stateString")
+    }
+
+    override fun onIsPlayingChanged(isPlaying: Boolean) {
+        val playingString: String = if (isPlaying) "PLAYING" else "NOT PLAYING"
+        Log.d(TAG, "Player is currently: $playingString")
+    }
+
+    override fun onRenderedFirstFrame() {
+        super.onRenderedFirstFrame()
+        // when first frame is rendered
+    }
+
+    override fun onEvents(player: Player, events: Player.Events) {
+        if (events.contains(Player.EVENT_PLAYBACK_STATE_CHANGED) || events.contains(Player.EVENT_IS_PLAYING_CHANGED)) {
+            // Update to the UI
+            Log.d(TAG, "Player UI needs to updated")
+        }
+
+        // Move to 5 secs in event of changing media track
+        if (events.contains(Player.EVENT_MEDIA_ITEM_TRANSITION)) {
+            player.seekTo(5000L)
+        }
     }
 }
